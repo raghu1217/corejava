e9cc96d0d06bee095d596e860b7d6b956c4e372a
raghu1217 <raghuveerareddy.ch@gmail.com>
2023-10-20 17:30:16 -0500

    first commit

---
+++ src/loops.java
@@ -0,0 +1,102 @@
+public class loops {
+
+    //for loop//
+    public static void main(String[] args) {
+        /*for (int i = 0; i <= 10; i++) {
+            System.out.println("value of i=" + i);
+
+        }
+    }
+}
+
+
+    //while loop//
+        int i = 0;
+        while (i<5)
+        {
+            System.out.println("value of i="+i);
+            i++;
+        }
+    }
+}
+
+
+//do while loop//
+        int i = 0;
+        do {
+            System.out.println(i);
+            i++;
+        }
+        while (i < 5);
+    }
+}
+        //if statement//
+        int[] i = {1, 4, 5, 6, 7, 8, 3, 5, 6, 78, 8, 8};
+        for (int a : i) {
+            System.out.println("value of i=" +a);
+            if(a==78){
+                break;
+            }
+        }
+    }
+}
+
+
+        //if else//
+        int a = 10;
+        int b = 20;
+        if (a > b)
+        {
+            System.out.print("value of a is greater");
+        }
+        else{
+            System.out.print("value of b is greater");
+        }
+    }
+}
+
+
+        //arrays//
+
+        String[] details = {"name:raghu", "place:abc", "DOB:788", "education:masters"};
+        for(String x:details){
+        System.out.println (details[1]);
+        System.out.println(x);}
+        System.out.println(details.length);
+    }}
+
+        //boolean//
+
+        boolean nameisraghu = true;
+        if (!nameisraghu) {
+            System.out.print("my name is raghu");
+        } else {
+            System.out.print("fake user id");
+        }
+    }
+}
+        // logical operators//
+
+        int topscore = 90;
+        int centpercent = 100;
+        int secondtopscore = 80;
+        if (topscore < 100 && topscore > secondtopscore) {
+            System.out.print("this is the top score");
+        }
+        if (topscore < 100 && topscore < secondtopscore) {
+            System.out.print("this is NOT the top score");
+        }
+        if (topscore < 100 || topscore < secondtopscore) {
+            System.out.print("this may top score");
+        }
+        if (topscore > 100 || topscore < secondtopscore) {
+            System.out.print("this is NOT the top score");
+        }
+    }
+    }*/
+
+
+
+
+
+
